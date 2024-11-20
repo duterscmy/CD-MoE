5c81fe643631d229f580a52f596b24b7c7f337a7
Mingyu Cao <1054527636@qq.com>
2024-11-20 18:48:54 +0800

    add: CD-MoE

---
+++ cd-moe/step_1_get_weight/get_weight.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import argparse
+import torch
+from accelerate import infer_auto_device_map, init_empty_weights, load_checkpoint_and_dispatch
+from transformers import AutoTokenizer, T5Tokenizer, AutoConfig, AutoModelForCausalLM, LogitsProcessorList, LogitsProcessor
+from typing import List, Optional
+import random
+import os
+import json
+#import shortuuid
+import time
+
+from transformers.variables import *
+
+
+def compute_ppl(model, tokenizer, input_strs, gen_kwargs,
+                add_special_tokens=True, split_special_tokens=False, output_only=True, verbose=False):
+
+    model = model.eval()
+
+    # Tokenization
+    def encode_text_batch(input_strs):
+        inputs = tokenizer.batch_encode_plus(input_strs,
+                                             padding='longest',
+                                             #  add_special_tokens=add_special_tokens,
+                                             #  split_special_tokens=split_special_tokens,
+                                             return_tensors="pt")
+        input_ids = inputs.input_ids.to(model.device)
+        attention_mask = inputs.attention_mask.to(model.device)
+        return input_ids
+
+    batch_size = 1  # 批处理大小
+    num_texts = len(input_strs)
+    loss_sum = 0.0
+
+    for i in range(0, len(input_strs), batch_size):
+        text_list_batch = input_strs[i:i+batch_size]
+        input_ids = encode_text_batch(text_list_batch)
+        with torch.no_grad():
+            outputs = model(input_ids, labels=input_ids)
+            loss = outputs.loss.mean()
+            print("mean loss {}".format(loss))
+        loss_sum += loss.item()
+        print("loss sum {}".format(loss_sum))
+
+    mean_loss = loss_sum / num_texts  # 计算整个数据集的损失均值
+    mean_ppl = torch.exp(torch.tensor(mean_loss))
+    return mean_ppl
+
+
+def apply_llama_chat_template(tokenizer, input_strs, sys_prompt):
+    # Use LLaMA's Chat Template(A bit diffrent from original one at the beginning part, we may correct it to the standard llama prompt template later)
+    # input_strs = [('user_input', 'user'), ('AI_response', 'assistant'), ...]
+    tokenizer.chat_template = "{% for message in messages %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'system' %}{{ '<<SYS>>\\n' + message['content'] + '\\n<</SYS>>\\n\\n' }}{% elif message['role'] == 'assistant' %}{{ ' '  + message['content'] + ' ' + eos_token }}{% endif %}{% endfor %}"
+    system_prompt = {'content': sys_prompt, 'role': 'system'}
+    chat = [system_prompt] + [{'content': input_str,
+                               'role': role} for input_str, role in input_strs]
+    input_str = tokenizer.apply_chat_template(chat,
+                                              tokenize=False,
+                                              add_generation_prompt=True)
+    return input_str
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", default="./moe_prune/data/questions.jsonl",
+                    help="calibration data")
+parser.add_argument("--output", default="./dynamic_weight.json",
+                    help="预计算的路由权重")
+parser.add_argument("--model", default="./qw27",
+                    help="模型路径")
+parser.add_argument("--score-mode", type=str, default="l1", help="层间对专家排序的指标")
+parser.add_argument("--batch-size", type=int, default=4, help="并行解码的样本数量")
+parser.add_argument("--num-layer", type=int, default=24, help="默认为qw16B层数")  # deepseek 27
+parser.add_argument("--num-expert", type=int, default=64, help="默认为qw16B专家数")
+parser.add_argument("--load-in-8bit", action="store_true", help="load in 8bit")
+
+
+args = parser.parse_args()
+
+pytorch_checkpoint_path = args.model
+# @param ["", "0", "0,1", "0,1,2"] {allow-input: true}
+available_gpu_ids_str = "0"
+memory_per_gpu = "48GiB"  # @param ["", "38GiB"] {allow-input: true}
+cpu_memory = '50GiB'  # @param ["50GiB"] {allow-input: true}
+model_dtype = 'bfloat16'  # @param ["float32", "bfloat16"]
+offload = False  # @param {type:"boolean"}
+
+if torch.cuda.is_available():
+    cuda_list = available_gpu_ids_str.split(',')
+else:
+    available_gpu_ids_str, memory_per_gpu = "", ""
+    model_dtype = "bfloat16"
+    cuda_list = []
+
+no_split_module_classes = "OpenMoeDecoderLayer"
+
+# 1. Allocate Devices for Inference
+available_memory = {int(cuda): memory_per_gpu for cuda in cuda_list}
+available_memory['cpu'] = cpu_memory
+print('Available Devices and Memory: ', available_memory)
+
+# 2. Load the Model (init with empty weight to save memory)
+config = AutoConfig.from_pretrained(pytorch_checkpoint_path, trust_remote_code=True)
+#weights_location = snapshot_download(repo_id=pytorch_checkpoint_path)
+with init_empty_weights():
+    model = AutoModelForCausalLM.from_config(config,
+                                             torch_dtype=eval(
+                                                 f'torch.{model_dtype}'),
+                                             trust_remote_code=True)
+print('Model dtype: ', model.dtype)
+device_map = infer_auto_device_map(model,
+                                   max_memory=available_memory,
+                                   no_split_module_classes=no_split_module_classes)
+print('Inferred Device Map: \n', device_map)
+
+
+if args.load_in_8bit:
+    model = AutoModelForCausalLM.from_pretrained(
+        pytorch_checkpoint_path,
+        # device_map=device_map,
+        # torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        load_in_8bit=True,
+    )
+else:
+    model = AutoModelForCausalLM.from_pretrained(
+        pytorch_checkpoint_path,
+        device_map=device_map,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+    )
+tokenizer = AutoTokenizer.from_pretrained(pytorch_checkpoint_path)
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    model.resize_token_embeddings(len(tokenizer))
+
+# read benchmark
+with open(args.input, 'r') as fp:
+    questions = []
+    for line in fp:
+        line = line.strip()
+        if line:
+            question = json.loads(line)
+            questions.append(question)
+try:
+    raw_questions = list(map(lambda x: x["turns"][0], questions))
+except:
+    raw_questions = list(map(lambda x: x["text"], questions))
+
+
+batch_size = args.batch_size
+score_mode = args.score_mode
+num_layer = args.num_layer
+num_expert = args.num_expert
+
+
+layer_num_list.append(num_layer)
+mean_ppl = compute_ppl(model, tokenizer, raw_questions, None)
+print("no prune mean_ppl {}".format(mean_ppl))
+
+new_expert_idx_to_info = {}
+for key, value in dynamic_weights.items():
+    new_key = "{}-{}".format(key[0], key[1])
+    ave_w = value[0] / value[1]
+    new_expert_idx_to_info[new_key] = [value[0], value[1], ave_w]
+
+output_filename = args.output
+json.dump(new_expert_idx_to_info, open(output_filename, 'w'))
+
+
